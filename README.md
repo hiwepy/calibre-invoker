08842bc0f3c59051092b63e02c120321a032420a
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2018-05-08 17:12:25 +0800

    Java 命令行封装对Calibre的调用实现：待完善

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# calibre-invoker
 calibre-invoker
+=========
+
+https://manual.calibre-ebook.com/generated/en/cli-index.html
+
+A Java based wrapper for the [Calibre](https://calibre-ebook.com) command line tool. 
+
+
+### Requirements
+------------
+**[Calibre](https://calibre-ebook.com) must be installed and working on your system.**
+
+### Maven Dependency
+
+``` xml
+<dependency>
+	<groupId>com.github.vindell</groupId>
+	<artifactId>calibre-invoker</artifactId>
+	<version>${project.version}</version>
+</dependency>
+```
+
+### Usage
+------------
+``` 
+```
---
+++ src/main/java/com/github/vindell/calibre/invoker/request/FetchEbookMetadataInvocationRequest.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2018, vindell (https://github.com/vindell).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.github.vindell.calibre.invoker.request;
+
+import java.io.File;
+
+public interface FetchEbookMetadataInvocationRequest extends InvocationRequest {
+
+	File getCoverFile();
+
+	public String getAllowedPlugin();
+
+	boolean isAuthors();
+
+	boolean isIsbn();
+
+	boolean isOpf();
+
+	boolean isTitle();
+
+	long getTimeout();
+
+	/**
+	 * Set the value of the {@code allowed-plugin} {@code true} if the argument
+	 * {@code --allowed-plugin} was specified, otherwise {@code false}
+	 */
+	InvocationRequest setAllowedPlugin(String allowedPlugin);
+
+	/**
+	 * Set the value of the {@code --authors, -a} {@code true} if the argument
+	 * {@code --authors, -a} was specified, otherwise {@code false}
+	 */
+	InvocationRequest setAuthors(boolean authors);
+
+	/**
+	 * Set the value of the {@code  --isbn, -i} {@code true} if the argument
+	 * {@code  --isbn, -i} was specified, otherwise {@code false}
+	 */
+	InvocationRequest setIsbn(boolean isbn);
+
+	/**
+	 * Specify a filename. The cover, if available, will be saved to it. Without this option, no cover will be downloaded.
+	 */
+	InvocationRequest setCoverFile(File coverFile);
+
+	/**
+	 * Set the value of the {@code --opf, -o} {@code true} if the argument
+	 * {@code --opf, -o} was specified, otherwise {@code false}
+	 */
+	InvocationRequest setOpf(boolean opf);
+
+	/**
+	 * Set the value of the {@code --timeout, -d} {@code true} if the argument
+	 * {@code  --timeout, -d} was specified, otherwise {@code false}
+	 */
+	InvocationRequest setTimeout(long timeout);
+
+	/**
+	 * Set the value of the {@code  --title, -t} {@code true} if the argument
+	 * {@code --title, -t} was specified, otherwise {@code false}
+	 */
+	InvocationRequest setTitle(boolean title);
+
+}
